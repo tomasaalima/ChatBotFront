a5b0512bd65ceda2a40a6cf834d6f7388ec61482
RicardoEspindol <ricardoespindola128@gmail.com>
2023-03-31 19:31:29 -0300

    start dev

---
+++ chat-bot/src/components/menu.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+
+function Menu(){
+    return (
+        <div className="bg-gray-900">
+            <ul className=" gap-10 flex">
+                <li>Home</li>
+                <li>Tutorial</li>
+                <li>Sobre</li>
+                <li>tyafwd</li>
+            </ul>
+        </div>
+    )
+}
+
+export default Menu;
---
+++ chat-bot/src/routes.jsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import Fix from './pages/pageEx';
+
+const routes = [
+    { path: '/', element: <Fix/> },
+  
+  ];
+  export default routes;
