f170d38d32a0b1c6a96ba4913037a3ec581e9f90
tomasaalima <tomaslima_20@outlook.com>
2023-04-11 14:39:20 -0300

    Applied modifications

--- chat-bot/src/index.js
+++ chat-bot/src/index.js
@@ -3,18 +3,10 @@ import ReactDOM from 'react-dom/client';
 import { createBrowserRouter, RouterProvider} from "react-router-dom";
 import "./App.css"
 import App from './App';
-
-<<<<<<< HEAD
 import Home from './pages/Home';
 import Sobre from './pages/About';
-import Tutorial from './pages/Tutorial';
 import ErrorPage from './pages/Error';
 import Chat from './pages/Chat';
-=======
-import Home from './pages/home';
-import Sobre from './pages/sobre';
-import ErrorPage from './pages/error';
->>>>>>> 41d87f89f2ab9ed52156d1cf3f06c2d1c32be4e5
 
 // const router = createBrowserRouter([
 //   {
@@ -45,17 +37,10 @@ const router = createBrowserRouter([
           path: "/sobre",
           element: <Sobre/>
         },
-<<<<<<< HEAD
-        {
-          path: "/tutorial",
-          element: <Tutorial/>
-        },
         {
           path: "/chat",
           element: <Chat/>
         }
-=======
->>>>>>> 41d87f89f2ab9ed52156d1cf3f06c2d1c32be4e5
     ]
   }
 ]);
