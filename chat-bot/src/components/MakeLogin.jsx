6224f0fe9954d402340a7836ffbfec6970eeaf27
RicardoEspindol <ricardoespindola128@gmail.com>
2023-04-11 19:52:07 -0300

    animation divs home and MakeLogin button

---
+++ chat-bot/src/components/MakeLogin.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function MakeLogin(){
+    return(
+        <button className="h-10 bg-green-600 flex items-center justify-center fixed text-white top-3 right-4 w-32 font-exo2 rounded-sm hover:bg-green-500">
+            Fazer login
+        </button>
+    )
+}
+
+export default MakeLogin;
