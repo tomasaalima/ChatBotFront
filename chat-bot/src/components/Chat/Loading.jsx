063430838e23fa0f9bbaf898ef54f3c3228d13f0
tomasaalima <tomasabdias22@gmail.com>
2023-04-30 01:42:10 -0300

    chat dialogs part - II (keywords)

---
+++ chat-bot/src/components/Chat/Loading.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+function Loading() {
+  return (
+    <>
+      <div
+        className="w-full h-7 bg-red-400 text-white text-center"
+      >
+        Erro ao se conectar com o servidor, tente novamente mais tarde..
+      </div>
+    </>
+  )
+}
+
+export default Loading;
--- chat-bot/src/pages/Chat.jsx
+++ chat-bot/src/pages/Chat.jsx
@@ -2,6 +2,7 @@ import React, { useState, useEffect }  from "react";
 import axios from "axios";
 import Message from "../components/Chat/Message";
 import TextBox from "../components/Chat/TextBox";
+import Loading from "../components/Chat/Loading";
 import Background from "../components/Background";
 import NavBar from "../components/NavBar";
 
@@ -9,7 +10,10 @@ import NavBar from "../components/NavBar";
 function Chat () {
   const [hello, setHello] = useState([]);
   const [conversation, setConversation] = useState([]);
-  const [userMSG, setUserMSG] = useState({'type':'', 'text': ''});
+  const [userMSG, setUserMSG] = useState({'type': '', 'text': ''});
+  const [ subject, setSubject ] = useState({'theme': '', 'length': 2});
+  const [ search, setSearch ] = useState();
+  const [ questions, setQuestions ] = useState([]);
   
   useEffect(() => {
     //Consultar mensagem de saudação
@@ -19,15 +23,63 @@ function Chat () {
   }, []);
 
   useEffect(() => {
-    setConversation([...conversation, dialogueGen(userMSG.type, userMSG.text)]);
+    if (userMSG.type !== '') {
+      setConversation([...conversation, dialogueGen(userMSG.type, userMSG.text)]);
+    }
   }, [userMSG]);
+
+  useEffect(() => {
+    if (subject.theme !== '') {
+      axios.get('http://127.0.0.1:8000/api/keywords/fluxo/hatunamatata ontem manha login acessar fluxo teste')
+    .then(response => setQuestions((response.data)));
+    }
+  }, [search]);
+
+  useEffect(() => {
+    if (conversation.length === subject.length) {
+      switch(userMSG.text) {
+        case '1' : {
+          setSubject({'theme': 'fluxo', 'length': subject.length});
+          break;
+        }
+        case '2' : {
+          setSubject({'theme': 'edital', 'length': subject.length});
+          break;
+        }
+        case '3' : {
+          setSubject({'theme': 'documentação', 'length': subject.length});
+          break;
+        }
+        default: {
+          setConversation([...conversation, dialogueGen("robot", "Digite: </br>'1' para falar sobre 'fluxo' </br>'2' para falar sobre 'editais' </br>'3' para falar sobre 'Documentação'")]);
+          setSubject({'theme': '', 'length': subject.length + 2});
+          break;
+      }
+      }
+    }
+  }, [conversation]);
   
   useEffect(() => {
-    if (hello.resposta != undefined) {
+    if (subject.theme !== '') {
+      setConversation([...conversation, dialogueGen('robot', 'Descreva de maneira breve o problema que está enfrentando')])
+    }
+  }, [subject]);
+
+  useEffect(() => {
+    if (hello.resposta !== undefined) {
       //Renderizar primeira mensagem
       setConversation([...conversation, dialogueGen("robot", hello.resposta)]);
-    }
+    } 
+    // else if (!hello.resposta) {
+    //   setConversation([...conversation, loading()]);
+    // }
   }, [hello.resposta]);
+
+  const loading = () => {
+    return (
+      <Loading/>
+    );
+  }
   
   const dialogueGen = (type, text) => {
     return (
