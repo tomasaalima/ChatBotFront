d65e76d1efb2ac2c85d327726b6bb9b8fc6ff976
tomasaalima <tomasabdias22@gmail.com>
2023-05-10 08:58:04 -0300

    bug fix

--- chat-bot/src/components/Video.jsx
+++ chat-bot/src/components/Video.jsx
@@ -5,10 +5,22 @@ function Video(props){
     const {description} = props;
 
     return (
-        <div className="h-5/6 w-5/6 m-auto flex items-center justify-center text-white flex-col">
-            <iframe  src={`${link}`} title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" 
-            allowfullscreen className="w-5/6 h-5/6"></iframe>
-            <p className="text-center text-footer font-error text-lg font-medium mt-1">{description}</p>
+        <div 
+            className="h-5/6 w-5/6 m-auto flex items-center justify-center text-white flex-col"
+        >
+            <iframe  
+                src={`${link}`} 
+                title="YouTube video player" 
+                frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" 
+                className="w-5/6 h-5/6"
+                allowfullscreen 
+            >   
+            </iframe>
+            <p 
+                className="text-center text-footer font-error text-lg font-medium mt-1"
+            >
+                {description}
+            </p>
         </div>
     )
 }
--- chat-bot/src/pages/VidAndImage.jsx
+++ chat-bot/src/pages/VidAndImage.jsx
@@ -19,47 +19,64 @@ function VidAndImage(){
         <>
         <NavBar/>
         <Background/>
-        <div className="absolute top-20 text-white flex justify-center w-full h-5/6">
-                <div className=" flex flex-col justify-center items-center w-8/12">
-                    <h1 className="font-oswald text-2xl mt-6">Aproveite nossos tutoriais!</h1>
-                    <div className="flex flex-col w-11/12 h-4/6 items-center justify-center mt-28">
-                        <nav className="w-full flex justify-center h-11 items-center top-3 divide-x-2 font-roboto bg-footer">
+        <div 
+            className="absolute top-20 text-white flex justify-center w-full h-5/6"
+        >
+                <div 
+                    className=" flex flex-col justify-center items-center w-8/12"
+                >
+                    <h1 
+                        className="font-oswald text-2xl mt-6"
+                    >
+                        Aproveite nossos tutoriais!
+                    </h1>
+                    <div 
+                        className="flex flex-col w-11/12 h-4/6 items-center justify-center mt-28"
+                    >
+                        <nav 
+                            className="w-full flex justify-center h-11 items-center top-3 divide-x-2 font-roboto bg-footer"
+                        >
                             <button  
-                            onClick={() => handleTabClick("tab1")}
-                            className={`${
-                                activeTab === "tab1"
-                                ? "bg-footer text-white"
-                                : "bg-slate-200 text-gray-900"
-                            } flex items-center justify-center w-2/4 font-roboto h-10`}
+                                onClick={() => handleTabClick("tab1")}
+                                className={`${
+                                    activeTab === "tab1"
+                                    ? "bg-footer text-white"
+                                    : "bg-slate-200 text-gray-900"
+                                } flex items-center justify-center w-2/4 font-roboto h-10`}
                             >
-                            VÍDEOS
+                                VÍDEOS
                             </button>
 
                             <button 
-                            onClick={() => handleTabClick("tab2")}
-                            className={`${
-                                activeTab === "tab2"
-                                ? "bg-footer text-white"
-                                : "bg-slate-200 text-gray-900"
-                            } flex items-center justify-center w-2/4 font-roboto h-10`}                            >
-                            MANUAIS
+                                onClick={() => handleTabClick("tab2")}
+                                className={`${
+                                    activeTab === "tab2"
+                                    ? "bg-footer text-white"
+                                    : "bg-slate-200 text-gray-900"
+                                } flex items-center justify-center w-2/4 font-roboto h-10`}                            >
+                                MANUAIS
                             </button>
                         </nav>
-                        <div className="h-full w-full  text-black bg-white">
-                        <div className="h-full w-full">
+                        <div 
+                            className="h-full w-full  text-black bg-white"
+                        >
+                        <div 
+                            className="h-full w-full"
+                        >
                             {activeTab === "tab1" && (
-                                <div className="h-full w-full grid grid-cols-2 gap-x-4 text-black">
+                                <div 
+                                    className="h-full w-full grid grid-cols-2 gap-x-4 text-black"
+                                >
                                     <Video link="https://www.youtube.com/embed/H2bKVI7eS4s" description="Joji"></Video>
-                                    <Video link="https://www.youtube.com/embed/wjEzE6HSgWo" description="ANALfabeto"></Video>
-                                
+                                    <Video link="https://www.youtube.com/embed/wjEzE6HSgWo" description="ANALfabeto"></Video> 
                                 </div>
                             )}
-                            {activeTab === "tab2"&& (
-                                <div className="h-full w-full grid grid-cols-2 gap-x-4 text-black">
+                            {activeTab === "tab2" && (
+                                <div 
+                                    className="h-full w-full grid grid-cols-2 gap-x-4 text-black"
+                                >
                                     <FrameEdital document="Permanencia 2023.1"></FrameEdital>
                                     <FrameEdital document="Permanencia 2023.2"></FrameEdital>
-
-                                
                                 </div>
                             )}
                         </div>
