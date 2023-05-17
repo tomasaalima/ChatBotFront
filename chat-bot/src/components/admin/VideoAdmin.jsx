87b5ff0f206e83789122a0e057fdef0c0487c5b8
RicardoEspindol <ricardoespindola128@gmail.com>
2023-05-16 22:38:45 -0300

    finish page admin's videos

--- chat-bot/src/components/admin/VideoAdmin.jsx
+++ chat-bot/src/components/admin/VideoAdmin.jsx
@@ -1,5 +1,8 @@
 import React, { useState } from "react";
-import "./VideoAdmin.css"; // Importe o arquivo CSS
+import "./VideoAdmin.css"; 
+import PopupAlert from "./PopupAlert";
+import PopupEdit from './PopupEdit';
+
 
 function VideoAdmin(props) {
   const { link } = props;
@@ -13,26 +16,51 @@ function VideoAdmin(props) {
     setShowButtons(false);
   };
 
+   //PopUp Alert
+   const [modalOpen, setModalOpen] = useState(false);
+   const handleOpenModal = () => {
+     setModalOpen(true);
+   };
+   const handleCloseModal = () => {
+     setModalOpen(false);
+   };
+
+   //PopUp Edit
+
+   const [editOpen, setEditOpen] = useState(false);
+   const abrirEdit = () => {
+       setEditOpen(true);
+   }
+   const fecharEdit = () => {
+       setEditOpen(false);
+   }
+
   return (
     <div
-      className="video-wrapper h-5/6 m-auto"
+      className="relative h-5/6 m-auto"
       onMouseEnter={handleMouseEnter}
       onMouseLeave={handleMouseLeave}
     >
       <iframe
         src={`${link}`}
-        title="YouTube video player"
-        frameBorder="0"
-        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+        title="YouTube video player"        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
         allowFullScreen
       ></iframe>
 
       {showButtons && (
         <div className="buttons-wrapper">
-          <button>Botão 1</button>
-          <button>Botão 2</button>
+          <button className="bg-redfooter text-white  w-2/4 h-8 font-exo2"
+          onClick={handleOpenModal}
+          >Remover</button>
+          <button className="bg-footer text-white  w-2/4 h-8 font-exo2"
+           onClick={abrirEdit}
+          >Editar</button>
         </div>
       )}
+      <PopupAlert isOpen={modalOpen} onClose={handleCloseModal} />
+      <PopupEdit isOpen={editOpen} onClose={fecharEdit} />
+
+
     </div>
   );
 }
